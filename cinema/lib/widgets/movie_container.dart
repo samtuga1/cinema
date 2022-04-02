9f9873b9f43025c23e15dafa31f7703a50b4b005
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 23:04:53 +0000

    Implemented movie container

--- cinema/lib/widgets/movie_container.dart
+++ cinema/lib/widgets/movie_container.dart
@@ -4,21 +4,38 @@ class MovieContainer extends StatelessWidget {
   final String imageUrl;
   final int rate;
   final String title;
+  final String duration;
   const MovieContainer(
       {Key? key,
       required this.imageUrl,
       required this.rate,
-      required this.title})
+      required this.title,
+      required this.duration,
+      })
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
       Container(
+        height: 60,
+        width: 38,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(13),
         ),
-        child: Image.network(imageUrl),
+        child: Image.network(imageUrl, fit: BoxFit.cover),
+      ),
+      Text(title),
+      Row(
+        children: [
+          Text('⭐ $rate'),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(duration),
+          ],
       )
     ]);
   }
