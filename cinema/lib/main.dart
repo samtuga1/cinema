6d59cb3ff39bfb87a9ab7eb45fc8b79ff6aaa815
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 20:53:57 +0000

    Added home screen in main file

--- cinema/lib/main.dart
+++ cinema/lib/main.dart
@@ -1,3 +1,4 @@
+import './screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       // ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
