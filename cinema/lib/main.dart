565ddde9f7884a07c297a4e91aee9416d95d8b83
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 20:56:25 +0000

    minor update

--- cinema/lib/main.dart
+++ cinema/lib/main.dart
@@ -11,12 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Cinema',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.blue,
-      // ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
