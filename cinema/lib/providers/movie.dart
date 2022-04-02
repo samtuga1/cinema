5a531488fbaf7ddeaf3ea891d665d1c8707dcda9
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 23:33:25 +0000

    Update

--- cinema/lib/providers/movie.dart
+++ cinema/lib/providers/movie.dart
@@ -13,9 +13,9 @@ class Movie {
   Movie({
     required this.title,
     required this.imageUrl,
-     this.description,
+    this.description,
     required this.rate,
-     this.casts,
+    this.casts,
     required this.duration,
   });
 }
@@ -23,23 +23,23 @@ class Movie {
 class Movies with ChangeNotifier {
   List<Movie> _movies = [
     Movie(
-      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      title: 'Ant-Man',
-      duration: '1hr 30min',
-      rate: 3
-    ),
+        imageUrl:
+            'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        title: 'Ant-Man',
+        duration: '1hr 30min',
+        rate: 3),
     Movie(
-      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      title: 'Ant-Man',
-      duration: '1hr 30min',
-      rate: 3
-    ),
+        imageUrl:
+            'https://images.pexels.com/photos/10594054/pexels-photo-10594054.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        title: 'Ant-Man',
+        duration: '1hr 30min',
+        rate: 3),
     Movie(
-      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      title: 'Ant-Man',
-      duration: '1hr 30min',
-      rate: 3
-    ),
+        imageUrl:
+            'https://images.pexels.com/photos/10025734/pexels-photo-10025734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        title: 'Ant-Man',
+        duration: '1hr 30min',
+        rate: 3),
   ];
 
   List<Movie> get movies {
