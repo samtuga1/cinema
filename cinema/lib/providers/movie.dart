b5b2cce6596712dfe0de9f8dc937abf4d650d1cd
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 22:45:57 +0000

    Added a lit of movies

--- cinema/lib/providers/movie.dart
+++ cinema/lib/providers/movie.dart
@@ -1,19 +1,48 @@
+import 'package:flutter/cupertino.dart';
+
 import '../providers/cast.dart';
 
 class Movie {
   final String title;
   final String imageUrl;
-  final String description;
+  final String? description;
   final int rate;
-  List<Cast> casts;
+  List<Cast>? casts;
   final String duration;
 
   Movie({
     required this.title,
     required this.imageUrl,
-    required this.description,
+     this.description,
     required this.rate,
-    required this.casts,
+     this.casts,
     required this.duration,
   });
+}
+
+class Movies with ChangeNotifier {
+  List<Movie> _movies = [
+    Movie(
+      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Ant-Man',
+      duration: '1hr 30min',
+      rate: 3
+    ),
+    Movie(
+      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Ant-Man',
+      duration: '1hr 30min',
+      rate: 3
+    ),
+    Movie(
+      imageUrl: 'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Ant-Man',
+      duration: '1hr 30min',
+      rate: 3
+    ),
+  ];
+
+  List<Movie> get movies {
+    return _movies;
+  }
 }
