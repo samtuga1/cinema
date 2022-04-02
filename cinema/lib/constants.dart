c12a761d45295bbc67eb4155d155903d79560870
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-02 23:44:03 +0000

    update my textstyles

--- cinema/lib/constants.dart
+++ cinema/lib/constants.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TextStyles{
+class TextStyles {
   static final textstyle = GoogleFonts.lexend();
+  static TextStyle text1 = textstyle.copyWith(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 22,
+  );
+  static TextStyle text2 = textstyle.copyWith(
+    color: Colors.white,
+    fontSize: 13.5,
+  );
 }
