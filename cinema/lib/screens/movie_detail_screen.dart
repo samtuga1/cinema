41fb7386955286e645e7da12610bdb97fdf7d560
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 20:47:18 +0000

    Update

--- cinema/lib/screens/movie_detail_screen.dart
+++ cinema/lib/screens/movie_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cinema/providers/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../constants.dart' as constants;
 
 class MovieDetailScreen extends StatelessWidget {
   static const routeName = '/movie_detail_screen';
@@ -10,9 +11,8 @@ class MovieDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
-    //final movieId = ModalRoute.of(context)?.settings.arguments as String;
-    //final movie = Provider.of<Movies>(context).findById(movieId);
-    //print(movie.title);
+    final movieId = ModalRoute.of(context)?.settings.arguments as String;
+    final movie = Provider.of<Movies>(context).findById(movieId);
     return Scaffold(
       body: Stack(
         overflow: Overflow.visible,
@@ -21,15 +21,41 @@ class MovieDetailScreen extends StatelessWidget {
             height: deviceHeight * 0.427,
             width: double.infinity,
             child: Image.network(
-              'https://images.pexels.com/photos/10410155/pexels-photo-10410155.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+              movie.imageUrl,
               fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.arrow_back_rounded)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
+              child: Column(children: [
+                Text(
+                  movie.title, style: constants.TextStyles.text1,
+                  textAlign: TextAlign.left,
+                  ),
+                  Wrap(
+                    children: List<Text>.generate(
+                    movie.rate.floor(), (index) => const Text('⭐'),
+                      ),)
+                ],),
               decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
+                color: Theme.of(context).scaffoldBackgroundColor,
                 borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(15),
                     topRight: Radius.circular(15)),
