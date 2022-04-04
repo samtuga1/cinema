04101d4225e27e150b3974c653757a3fc3bc58e6
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 21:05:44 +0000

    Updated my movie constructors

--- cinema/lib/providers/movie.dart
+++ cinema/lib/providers/movie.dart
@@ -25,26 +25,52 @@ class Movie {
 class Movies with ChangeNotifier {
   List<Movie> _movies = [
     Movie(
+      casts: [
+        Cast(
+            name: 'Sam Twum',
+            imageUrl:
+                'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+      ],
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       id: 'm1',
-        imageUrl:
-            'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        title: 'Ant-Man',
-        duration: '1hr 30min',
-        rate: 3),
+      imageUrl:
+          'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Ant-Man',
+      duration: '1hr 30min',
+      rate: 3,
+    ),
     Movie(
-      id: 'm2',
+        casts: [
+          Cast(
+              name: 'Sam Twum',
+              imageUrl:
+                  'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+        ],
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        id: 'm2',
         imageUrl:
             'https://images.pexels.com/photos/10594054/pexels-photo-10594054.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         title: 'Ant-Man',
         duration: '1hr 30min',
         rate: 3),
     Movie(
+      casts: [
+        Cast(
+            name: 'Sam Twum',
+            imageUrl:
+                'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+      ],
+      description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       id: 'm3',
-        imageUrl:
-            'https://images.pexels.com/photos/10025734/pexels-photo-10025734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        title: 'Ant-Man',
-        duration: '1hr 30min',
-        rate: 3),
+      imageUrl:
+          'https://images.pexels.com/photos/10025734/pexels-photo-10025734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Ant-Man',
+      duration: '1hr 30min',
+      rate: 3,
+    ),
   ];
 
   List<Movie> get movies {
@@ -52,6 +78,6 @@ class Movies with ChangeNotifier {
   }
 
   Movie findById(String? id) {
-   return _movies.firstWhere((movie) => movie.id == id);
+    return _movies.firstWhere((movie) => movie.id == id);
   }
 }
