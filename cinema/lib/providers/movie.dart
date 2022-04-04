4996667d19e5898aebb12a89be6777c126f309ff
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 15:09:23 +0000

    Minor

--- cinema/lib/providers/movie.dart
+++ cinema/lib/providers/movie.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import '../providers/cast.dart';
 
 class Movie {
+  final String? id;
   final String title;
   final String imageUrl;
   final String? description;
@@ -11,6 +12,7 @@ class Movie {
   final String duration;
 
   Movie({
+    this.id,
     required this.title,
     required this.imageUrl,
     this.description,
@@ -23,18 +25,21 @@ class Movie {
 class Movies with ChangeNotifier {
   List<Movie> _movies = [
     Movie(
+      id: 'm1',
         imageUrl:
             'https://images.pexels.com/photos/11387289/pexels-photo-11387289.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         title: 'Ant-Man',
         duration: '1hr 30min',
         rate: 3),
     Movie(
+      id: 'm2',
         imageUrl:
             'https://images.pexels.com/photos/10594054/pexels-photo-10594054.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         title: 'Ant-Man',
         duration: '1hr 30min',
         rate: 3),
     Movie(
+      id: 'm3',
         imageUrl:
             'https://images.pexels.com/photos/10025734/pexels-photo-10025734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         title: 'Ant-Man',
@@ -44,5 +49,9 @@ class Movies with ChangeNotifier {
 
   List<Movie> get movies {
     return _movies;
+  }
+
+  Movie findById(String? id) {
+   return _movies.firstWhere((movie) => movie.id == id);
   }
 }
