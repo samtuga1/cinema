8c2d77bcadd6e70b5dc3e14955dcb716635ce307
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 20:25:51 +0000

    Update

--- cinema/lib/screens/home_screen.dart
+++ cinema/lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:card_swiper/card_swiper.dart';
-import 'package:cinema/providers/movie_chip.dart';
 import '../constants.dart' as constants;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +15,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final movieData = Provider.of<Movies>(context);
-    final movieChips = Provider.of<MoviesChips>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -31,78 +29,6 @@ class HomeScreen extends StatelessWidget {
         child: ListView(
           children: [
             const MovieChipContainer(),
-            // Container(
-            //     margin: const EdgeInsets.only(top: 12),
-            //     height: 40,
-            //     child: ListView(
-            //       children: [
-            //         Container(
-            //           margin: const EdgeInsets.only(right: 12),
-            //           child: ChoiceChip(
-            //             selectedColor: Colors.purple,
-            //             onSelected: ((value) {
-            //               _isSelected = value;
-            //             }),
-            //             selected: _isSelected,
-            //             label: Padding(
-            //               padding: const EdgeInsets.all(6.0),
-            //               child: Text(
-            //                 'All',
-            //                 style: constants.TextStyles.text2,
-            //               ),
-            //             ),
-            //           ),
-            //         )
-            //       ],
-            //     )),
-            // Padding(
-            //padding: const EdgeInsets.only(),
-            //child:
-            // Row(
-            //   children: [
-            //     IconButton(
-            //       icon: const Icon(Icons.menu),
-            //       onPressed: () {},
-            //     ),
-            //     const SizedBox(
-            //       width: 40,
-            //     ),
-            //     Expanded(
-            //       child: Container(
-            //         decoration: BoxDecoration(
-            //           borderRadius: BorderRadius.circular(7),
-            //           color: Colors.black38,
-            //         ),
-            //         height: 44,
-            //         child: const TextField(
-            //           cursorColor: Colors.grey,
-            //           decoration: InputDecoration(
-            //             border: OutlineInputBorder(
-            //               borderSide: BorderSide.none,
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     ),
-            //     Padding(
-            //       padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            //       child: Container(
-            //         decoration: BoxDecoration(
-            //           color: Colors.black12,
-            //           borderRadius: BorderRadius.circular(12),
-            //         ),
-            //         padding: const EdgeInsets.all(3.5),
-            //         child: IconButton(
-            //           splashRadius: 26,
-            //           color: Colors.grey,
-            //           icon: const Icon(Icons.search),
-            //           onPressed: () {},
-            //         ),
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            //),
             movieType('Top Trends'),
             SizedBox(
               height: 195,
