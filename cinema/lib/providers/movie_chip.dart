105723e86856fdf9040d6a3cb8b31876dcf65f84
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 19:43:38 +0000

    Update

--- cinema/lib/providers/movie_chip.dart
+++ cinema/lib/providers/movie_chip.dart
@@ -2,16 +2,23 @@ import 'package:flutter/foundation.dart';
 
 class MovieChip {
   final String? name;
-  final int? index;
+  int? chipIndex;
 
-  MovieChip(this.name, this.index);
+  MovieChip({
+    this.name,
+    this.chipIndex,
+  });
 }
 
 class MoviesChips with ChangeNotifier {
-  List<MovieChip> _chips = [];
+  final List<MovieChip> _chips = [
+    MovieChip(name: 'All', chipIndex: 0),
+    MovieChip(name: 'Action', chipIndex: 1),
+    MovieChip(name: 'Adventure', chipIndex: 2),
+    MovieChip(name: 'Series', chipIndex: 3),
+  ];
 
   List<MovieChip> get chips {
     return _chips;
   }
-
 }
