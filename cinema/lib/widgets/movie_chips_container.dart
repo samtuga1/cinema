7984bd6727cd2a713e484fb2b8d2a96d1b70c29c
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-04 19:43:51 +0000

    Update

--- cinema/lib/widgets/movie_chips_container.dart
+++ cinema/lib/widgets/movie_chips_container.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import '../constants.dart' as constants;
 
 class MovieChipContainer extends StatefulWidget {
-  final String? name;
-  final int? chipIndex;
   const MovieChipContainer({
     Key? key,
-    required this.chipIndex,
-    required this.name,
   }) : super(key: key);
 
   @override
@@ -15,26 +11,34 @@ class MovieChipContainer extends StatefulWidget {
 }
 
 class _MovieChipsState extends State<MovieChipContainer> {
-
   bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 12),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(22),
-        onTap: () {},
-        child: Chip(
-          backgroundColor: isSelected ? Colors.purple : Colors.grey,
-          label: Padding(
-            padding: const EdgeInsets.all(6.0),
-            child: Text(
-              widget.name!,
-              style: constants.TextStyles.text2,
-            ),
-          ),
-        ),
+    return SizedBox(
+      height: 40,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 4,
+        itemBuilder: ((context, index) => Container(
+              margin: const EdgeInsets.only(right: 12),
+              child: ChoiceChip(
+                selectedColor: Colors.purple,
+                selected: isSelected,
+                onSelected: (bool value) {
+                  setState(() {
+                    value = isSelected;
+                  });
+                },
+                label: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Text(
+                    'All',
+                    style: constants.TextStyles.text2,
+                  ),
+                ),
+              ),
+            )),
       ),
     );
   }
