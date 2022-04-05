8b954b31a9b0efc195e6b318a3adb2fcf8fbafe9
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-05 03:28:26 +0000

    update

---
+++ cinema/lib/widgets/menu_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => IconButton(
+      icon: Icon(Icons.menu),
+      onPressed: () => ZoomDrawer.of(context)!.toggle(),
+    );
+}
