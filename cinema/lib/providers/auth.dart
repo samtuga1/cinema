a7cea3e3aa849200878e6f8c9a77dc1ec3216e95
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-04-16 14:58:00 +0000

    update

---
+++ cinema/lib/providers/auth.dart
@@ -0,0 +1,59 @@
+import 'package:cinema/models/http_exception.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String? _token;
+  DateTime? _expiryDate;
+  String? _userId;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String? get token {
+    if (_token != null &&
+        _expiryDate != null &&
+        _expiryDate!.isAfter(DateTime.now())) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> authenticate(
+      String email, String password, String urlSegment) async {
+    const _apiKey = 'AIzaSyAKGd9fKPfJdiSv2Ew1nVI0qhue7ZzJ8Ow';
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_apiKey';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(
+            responseData['expiresIn'],
+          ),
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      print(error);
+    }
+  }
+}
